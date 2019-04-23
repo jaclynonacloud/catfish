531fdb215f920d4860ac1e88ab8002380360177f
Jaclyn Staples <jaclynonacloud@gmail.com>
2019-04-23 13:57:06 -0300

    Made Peeper spritesheet loop.

--- public/assets/spritesheets/Peeper_Spritesheet.js
+++ public/assets/spritesheets/Peeper_Spritesheet.js
@@ -2,7 +2,7 @@
   Peeper_Spritesheet = function() {
     this.initialize();
   }
-  Peeper_Spritesheet._SpriteSheet = new createjs.SpriteSheet({"images":["assets/spritesheets/Peeper_Spritesheet.png"],"frames":[[254,0,127,112],[0,224,127,112],[127,112,127,112],[127,0,127,112],[0,112,127,112],[0,0,127,112]],"animations":{"fishIdle":{"frames":[0,0,0,0,0,0,1,1,1,1,1,1,2,2,2,2,2,2,1,1,1,1,1,1],"next":false},"fishPanic":{"frames":[3,3,4,4,5,5,4,4],"loop":true}}});
+  Peeper_Spritesheet._SpriteSheet = new createjs.SpriteSheet({"images":["assets/spritesheets/Peeper_Spritesheet.png"],"frames":[[254,0,127,112],[0,224,127,112],[127,112,127,112],[127,0,127,112],[0,112,127,112],[0,0,127,112]],"animations":{"fishIdle":{"frames":[0,0,0,0,0,0,1,1,1,1,1,1,2,2,2,2,2,2,1,1,1,1,1,1]},"fishPanic":{"frames":[3,3,4,4,5,5,4,4]}}});
   var Peeper_Spritesheet_p = Peeper_Spritesheet.prototype = new createjs.Sprite();
   Peeper_Spritesheet_p.Sprite_initialize = Peeper_Spritesheet_p.initialize;
   Peeper_Spritesheet_p.initialize = function() {
@@ -11,3 +11,7 @@
   }
   window.Peeper_Spritesheet = Peeper_Spritesheet;
 }(window))
+
+
+
+//"next":false
